ff0a62172e9aab6bb98e0836504107e94c52d3c0
Carlos León <aahtty@espol.edu.ec>
2017-12-15 13:05:39 -0500

    Correcciones de la clase

---
+++ Mockito/src/main/java/Multiplicacion.java
@@ -0,0 +1,35 @@
+
+
+public class Multiplicacion {
+	
+	private int resultado;
+	
+	public Multiplicacion() {
+		this.resultado=0;
+	}
+	
+	public int multiplicar(int multiplicando, int multiplicador) {
+		int temp;
+		if(multiplicador==0) {
+			this.resultado=0;
+			return this.resultado=0;
+		}
+		if(multiplicador<0) {
+			//suma.new Suma();
+			//resta=new Resta();
+			//multiplicador=resta.restar(0,multplicador);
+			for(int i=0;i<multiplicador;i++) {
+				//this.resultado=suma.sumar(this.resultado,multiplicando);
+			}
+			//this.resultado=suma.sumar(this.resultado,-1);
+		}
+		else {
+			for(int i=0;i<multiplicador;i++) {
+				//this.resultado=suma.sumar(this.resultado,multiplicando);
+			}
+		}
+		
+		return this.resultado;
+	}
+	
+}
