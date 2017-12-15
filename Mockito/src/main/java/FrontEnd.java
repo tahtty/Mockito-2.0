412d4349b4b14395fdfafaa5f574a914d38918f8
Carlos León <carlos.leon.moran@gmail.com>
2017-12-15 12:41:40 -0500

    Esquema de Frontend

--- Mockito/src/main/java/FrontEnd.java
+++ Mockito/src/main/java/FrontEnd.java
@@ -4,6 +4,7 @@ public class FrontEnd {
 	public static void main(String[] args) {
 		String opcionS;
 		int opcion;
+		int resultado;
 		Scanner sc;
 		boolean bandera=true;
 		// TODO Auto-generated method stub
@@ -13,28 +14,39 @@ public class FrontEnd {
 		System.out.println("2) Multiplicación");
 		System.out.println("3) Resta");
 		do {
-			sc=new Scanner(System.in);
-			opcionS=sc.nextLine();
-			try {
-	            opcion = Integer.parseInt(opcionS);
-	            switch(opcion) {
-	            	case 1:
-	            		break;
-	            	case 2:
-	            		break;
-	            	case 3:
-	            		break;
-	            	default:
-	            		System.out.println("Ingrese una opción válida");
-	            }
-	            
-	        } catch (NumberFormatException excepcion) {
-	            bandera = true;
-	            System.out.println("Ingrese un número");
-	        }
+			sc= new Scanner(System.in);
+			opcionS = sc.nextLine();
+			if(FrontEnd.isNumeric(opcionS)) {
+				opcion= Integer.parseInt(opcionS);
+				switch(opcion) {
+				case 1:
+					resultado =0;//Igualo a la suma
+					System.out.println("El resultado es: "+String.valueOf(resultado));
+					break;
+				case 2:
+					resultado =0;//Igualo a la multiplicación
+					System.out.println("El resultado es: "+String.valueOf(resultado));
+					break;
+				case 3:
+					resultado =0;//Igualo a la resta
+					System.out.println("El resultado es: "+String.valueOf(resultado));
+					break;
+				default:
+					System.out.println("Ingrese una opción válida");
+					break;
+				}
+			}
 		}while(bandera);
 		
 	}
 	 
+	private static boolean isNumeric(String cadena){
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
+	}
 
 }
