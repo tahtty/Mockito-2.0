bea35202d1dfae61dffe68f859640cc8fdf67e89
Juan <jealvarado@gmail.com>
2018-02-15 23:23:53 -0500

    Clases FrontEnd actualizada

--- Mockito/src/main/java/FrontEnd.java
+++ Mockito/src/main/java/FrontEnd.java
@@ -5,6 +5,7 @@ public class FrontEnd {
         static OperacionSuma suma ;
         static OperacionMultiplicacion multiplicacion;
         static OperacionResta resta;
+        static OperacionDivision division;
 
         public static void setSuma(OperacionSuma suma) {
             FrontEnd.suma = suma;
@@ -28,33 +29,38 @@ public class FrontEnd {
             System.out.println("Calculadora");
             do{
                 do{
-                    System.out.println("Ingrese la operaciÃ³n que desea realizar");
+                    System.out.println("Ingrese la operación que desea realizar");
                     System.out.println("1) Suma");
-                    System.out.println("2) MultiplicaciÃ³n");
+                    System.out.println("2) Multiplicación");
                     System.out.println("3) Resta");
+                    System.out.println("4) División");
                     sc = new Scanner(System.in);
                     opS = sc.nextLine();
                     v1= FrontEnd.isNumeric(opS);
                 }while(!v1);
                 v1=true;
                 op=Integer.parseInt(opS);
-                v1=isInRange(op,0,4);
+                v1=isInRange(op,0,5);
             }while(!v1);
             do{
-                System.out.println("Ingrese el primer nÃºmero:");
+                System.out.println("Ingrese el primer número:");
                 sc = new Scanner(System.in);
                 opS = sc.nextLine();
                 v1= FrontEnd.isNumeric(opS);
                 num1=Integer.parseInt(opS);
             }while(!v1);
             do{
-                System.out.println("Ingrese el segundo nÃºmero:");
+                System.out.println("Ingrese el segundo número:");
                 sc = new Scanner(System.in);
                 opS = sc.nextLine();
                 v1= FrontEnd.isNumeric(opS);
                 num2=Integer.parseInt(opS);
             }while(!v1);
-            int resultado=FrontEnd.calcular(op, num1, num2);
+            String resultado=FrontEnd.calcular(op, num1, num2);
+            if(resultado.contains(":")) {
+            	String[]division= resultado.split("/");
+            	resultado = "Cociente: "+division[0]+" Residuo: "+division[1];
+            }
             System.out.println("El resultado es: "+resultado);
         }
 		
@@ -71,20 +77,23 @@ public class FrontEnd {
             return(num>inicio&&num<fin);
         }
 
-        public static int calcular(int op, int num1, int num2){
-            int resultado =0;
+        public static String calcular(int op, int num1, int num2){
+            String resultado ="";
             switch (op){
                 case 1:
-                    resultado = suma.sumar(num1, num2);
+                    resultado = new Integer(suma.sumar(num1, num2)).toString();
                     break;
                 case 2:
-                    resultado = multiplicacion.multiplicar(num1, num2);
+                    resultado = new Integer(multiplicacion.multiplicar(num1, num2)).toString();
                     break;
                 case 3:
-                    resultado = resta.restar(num1, num2);
+                    resultado = new Integer(resta.restar(num1, num2)).toString();
+                    break;
+                case 4:
+                    resultado = division.dividir(num1, num2);
                     break;
                 default:
-                	resultado = -1;
+                	resultado = "-1";
                     break;
             }
             return resultado;
