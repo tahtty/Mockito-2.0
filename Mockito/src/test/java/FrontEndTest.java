dcd15b8b5443283588a5b67d9163f561b1e47150
Carlos Leon <carlos.leon.moran@gmail.com>
2018-02-15 23:35:09 -0500

    Test para operacion del FrontEnd

--- Mockito/src/test/java/FrontEndTest.java
+++ Mockito/src/test/java/FrontEndTest.java
@@ -10,14 +10,17 @@ public class FrontEndTest {
 		Suma suma = new Suma();
 		Resta rest = new Resta();
 		Multiplicacion multi =new Multiplicacion();
-		
+		OperacionDivision divi = mock(OperacionDivision.class);
+		when(divi.dividir(3, 3)).thenReturn("1/0");
 		rest.setSuma(suma);
 		multi.setSuma(suma);
 		multi.setResta(rest);
 		
+		
 		FrontEnd.setSuma(suma);
         FrontEnd.setResta(rest);
         FrontEnd.setMultiplicacion(multi);
+        FrontEnd.setDivision(divi);
 	}
 	@Test
 	public void test() {
@@ -26,9 +29,11 @@ public class FrontEndTest {
         assertTrue("Deber�a saler verdadero el n�mero",FrontEnd.isInRange(1,0,4));
         assertFalse("Deber�a saler falso",FrontEnd.isInRange(5,0,4));
         assertFalse("Deber�a saler falso",FrontEnd.isInRange(0,0,4));
-        assertEquals("Deber�a ser 6",6,FrontEnd.calcular(1, 3, 3));
-        assertEquals("Deber�a ser 9",5,FrontEnd.calcular(2, -5, -1));
-        assertEquals("Deber�a ser 0",0,FrontEnd.calcular(3, 3, 3));
+        assertEquals("Deber�a ser 6","6",FrontEnd.calcular(1, 3, 3));
+        assertEquals("Deber�a ser 9","5",FrontEnd.calcular(2, -5, -1));
+        assertEquals("Deber�a ser 0","0",FrontEnd.calcular(3, 3, 3));
+        assertEquals("Deber�a ser 1/0","1/0",FrontEnd.calcular(4, 3, 3));
 	}
 
 }
+	
